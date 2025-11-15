bcf692f1fb1c9143866d0b2b457cf85dcc29dad4
laurivuori <laurivuori.lv@gmail.com>
2025-11-15 12:58:49 +0200

    switch interrupt, somewhat finished orientation, uart write, add_accel_value, get_avg, orientation_from_accel

--- src/main.c
+++ src/main.c
@@ -41,7 +41,7 @@ TODO:
 
 #define DEFAULT_STACK_SIZE 2048
 #define CDC_ITF_TX      1
-#define MAX_BUFFER_SIZE 32
+#define MAX_BUFFER_SIZE 100
 #define COM_PORT_7  1
 #define BUFFER_SIZE 100
 
@@ -51,19 +51,20 @@ TODO:
 /* Global variables */
 uint8_t rx_buffer[MAX_BUFFER_SIZE];
 uint8_t tx_buffer[MAX_BUFFER_SIZE];
-
+uint8_t last_program_state = 0;
+uint8_t switch_to_gyro_task = 0;
 /**
- * @struct MorseCode
+ * @struct morse_code
  * @brief Represents a mapping between a letter and its Morse code.
- * This struct is used in the global morseTable array. {'A', ".-"}, {'B', "-..."},....
+ * This struct is used in the global morse_table array. {'A', ".-"}, {'B', "-..."},....
  */
-struct MorseCode {
+struct morse_code {
     char letter;
     const char *code;
 };
 
 
-struct MorseCode morseTable[] = {
+struct morse_code morse_table[] = {
     {'A', ".-"}, {'B', "-..."}, {'C', "-.-."}, {'D', "-.."},
     {'E', "."}, {'F', "..-."}, {'G', "--."}, {'H', "...."},
     {'I', ".."}, {'J', ".---"}, {'K', "-.-"}, {'L', ".-.."},
@@ -80,53 +81,54 @@ enum state {WAITING=1,
             PRINT_DATA=3,
             READ_UART=4,
             READ_GYRO=5,
-            WRITE_UART=6
+            WRITE_UART=6,
+            MENU_TASK=7
            };
-enum state programState = WAITING;
+enum state program_state = WAITING;
 
 void morse_to_text(const char *message, char *output){
-    int tableSize = sizeof(morseTable) / sizeof(morseTable[0]);
+    int table_size = sizeof(morse_table) / sizeof(morse_table[0]);
     char temp[BUFFER_SIZE];
-    int tempIndex = 0;
+    int temp_index = 0;
     int output_index = 0;
     output[0] = '\0'; // make sure output is empty
-    int spaceCount = 0; // to store spaces from message and determinen where words end or start
+    int space_count = 0; // to store spaces from message and determinen where words end or start
 
     for (int i = 0; message[i] != '\0'; i++) {
         //char charFromMessage = message[i];
         if (message[i] == '-' || message[i] == '.'){
-            if (tempIndex < sizeof(temp) - 1) {
-                temp[tempIndex] = message[i];
-                tempIndex++;
-                temp[tempIndex] = '\0'; //move end of line
+            if (temp_index < sizeof(temp) - 1) {
+                temp[temp_index] = message[i];
+                temp_index++;
+                temp[temp_index] = '\0'; //move end of line
             }
-            spaceCount = 0; // reset space count after a dot/dash
+            space_count = 0; // reset space count after a dot/dash
         }
         else if (message[i] == ' '){
-            spaceCount++;
-            printf("spacecount: %d\n", spaceCount);
+            space_count++;
+            printf("space_count: %d\n", space_count);
         }
-        if (spaceCount == 1 && tempIndex > 0){
+        if (space_count == 1 && temp_index > 0){
             // end of letter
-            for (int j = 0; j < tableSize; j++) {
-                if (strcmp(temp, morseTable[j].code) == 0) {
-                    output[output_index] = morseTable[j].letter;
+            for (int j = 0; j < table_size; j++) {
+                if (strcmp(temp, morse_table[j].code) == 0) {
+                    output[output_index] = morse_table[j].letter;
                     output_index++;
                 }
             }
-            tempIndex = 0;
+            temp_index = 0;
             temp[0] = '\0';
            
         }
-        else if (spaceCount == 2 && message[i+1] != ' '){
+        else if (space_count == 2 && message[i+1] != ' '){
             output[output_index] = ' ';
             output_index++;
-            spaceCount = 0; // reset space count after processing space
+            space_count = 0; // reset space count after processing space
         }
         else{
             // end of word
             output[output_index] = '\0'; // null-terminate the output string
-            spaceCount = 0; // reset space count after processing word
+            space_count = 0; // reset space count after processing word
         }
         
     }
@@ -169,13 +171,23 @@ void debug_print(const char* msg){
     usb_serial_print(msg);
     #endif
 }
-static void btn_fxn(uint gpio, uint32_t eventMask) {
-    // Tehtävä 1: Vaihda LEDin tila.
-    //            Tarkista SDK, ja jos et löydä vastaavaa funktiota, sinun täytyy toteuttaa se itse.
-    // Exercise 1: Toggle the LED. 
-    //             Check the SDK and if you do not find a function you would need to implement it yourself. 
-    // toggle_red_led();
+static void right_button_gyrotask_fxn_switch_interrupt(uint gpio, uint32_t eventMask) {
+    static TickType_t last_press = 0;
+    TickType_t cur_time = xTaskGetTickCountFromISR();
+
+    // 100ms debounce
+    if (cur_time - last_press < pdMS_TO_TICKS(200)) {
+        return; // ignore bounce
+    }
+    last_press = cur_time;
     toggle_led();
+    switch_to_gyro_task = !switch_to_gyro_task;
+    if (switch_to_gyro_task){
+        program_state = READ_GYRO;
+    }
+    else {
+        program_state = WAITING;
+    }
 
 
 }
@@ -193,7 +205,7 @@ static void usbTask(void *arg) {
 static void sensor_task(void *arg){
     (void)arg;
     for(;;){
-        // if (programState == READ_SENSOR){
+        // if (program_state == READ_SENSOR){
 
         // }
 
@@ -202,11 +214,17 @@ static void sensor_task(void *arg){
         vTaskDelay(pdMS_TO_TICKS(1000));
     }
 }
-static void print_task(void *arg){
+static void uart_write_task(void *arg){
     (void)arg;
 
     while(1){
-        
+        if (program_state == WRITE_UART){
+            tud_cdc_n_write(COM_PORT_7, tx_buffer, strlen(tx_buffer));
+            usb_serial_print(tx_buffer);
+            tud_cdc_n_write_flush(COM_PORT_7);
+            usb_serial_print("testing write uart task\n");
+            program_state = last_program_state;   
+        }
        
 
         // Do not remove this
@@ -224,6 +242,7 @@ static void display_task(void *arg){
     while(1){
         
         while (1) {
+
                 clear_display();
                 // write_text("Hello");
                 // draw_circle(1, 1, 20);
@@ -260,7 +279,7 @@ static void uart_receive_task(void *arg){
             vTaskDelay(pdMS_TO_TICKS(50));
     }
     for(;;){
-        if (programState == READ_UART){
+        if (program_state == READ_UART){
             // Get the number of bytes available for reading
             if(tud_cdc_n_available(COM_PORT_7) > 0) {
                 uint32_t count = tud_cdc_n_read(COM_PORT_7, rx_buffer, sizeof(rx_buffer));
@@ -294,9 +313,67 @@ static void uart_receive_task(void *arg){
         vTaskDelay(pdMS_TO_TICKS(1000));
     }
 }
+/****************************** */
+
+#define MAX_SAMPLES 3
+// --- buffer for last 3 accel samples ---
+float ax_buffer[MAX_SAMPLES] = {0};
+float ay_buffer[MAX_SAMPLES] = {0};
+float az_buffer[MAX_SAMPLES] = {0};
+int buffer_index = 0;
+int buf_filled = 0;
 
 
+/**
+ * 
+ * @brief Add new accel sample to buffer and loop around if buffer is full
+ * 
+ */
 
+void add_accel_value(float ax, float ay, float az) {
+    ax_buffer[buffer_index] = ax;
+    ay_buffer[buffer_index] = ay;
+    az_buffer[buffer_index] = az;
+
+    buffer_index = (buffer_index + 1) % MAX_SAMPLES;
+    if (buf_filled < MAX_SAMPLES) {
+        buf_filled++;
+    }
+}
+
+/**
+ * 
+ * @brief Get average of the accel samples in the buffer
+ * 
+ */
+void get_avg(float *ax, float *ay, float *az) {
+    float sx = 0, sy = 0, sz = 0;
+
+    for (int i = 0; i < buf_filled; i++) {
+        sx += ax_buffer[i];
+        sy += ay_buffer[i];
+        sz += az_buffer[i];
+    }
+
+    *ax = sx / buf_filled;
+    *ay = sy / buf_filled;
+    *az = sz / buf_filled;
+}
+
+// simple orientation detection
+int orientation_from_accel(float ax, float ay, float az) {
+    float TH = 0.5; // 50% of gravity
+
+    if (az > TH) return 1;   // face up
+    if (az < -TH) return 2;  // face down
+
+    if (ax > TH) return 3;   // on left side
+    if (ax < -TH) return 4;  // on right side
+    return 0; // unknown
+}
+
+
+/**************************** */
 // Tasks prints Accel: X=-0.57, Y=-0.01, Z=-0.83 | Gyro: X=-0.21, Y=0.63, Z=-0.17| Temp: 30.31°C
 static void gyroscope_task(void *arg){
     (void)arg;
@@ -313,10 +390,41 @@ static void gyroscope_task(void *arg){
     // Start collection data here. Infinite loop. 
     uint8_t buf[BUFFER_SIZE];
     for(;;){
-        if (programState == READ_GYRO){
+        if (program_state == READ_GYRO){
             if (ICM42670_read_sensor_data(&ax, &ay, &az, &gx, &gy, &gz, &t) == 0) {
-                sprintf(buf,"Accel: X=%.2f, Y=%.2f, Z=%.2f | Gyro: X=%.2f, Y=%.2f, Z=%.2f| Temp: %2.2f°C\n", ax, ay, az, gx, gy, gz, t);
-                usb_serial_print(buf);
+                // sprintf(buf,"Accel: X=%.2f, Y=%.2f, Z=%.2f | Gyro: X=%.2f, Y=%.2f, Z=%.2f| Temp: %2.2f°C\n", ax, ay, az, gx, gy, gz, t);
+                // sprintf(buf,"Accel: X=%.2f, Y=%.2f, Z=%.2f | Gyro: X=%.2f, Y=%.2f, Z=%.2f\n", ax, ay, az, gx, gy, gz);
+                // sprintf(buf,"aX:%.2f, aY:%.2f, aZ:%.2f\n", ax, ay, az);
+                //sprintf(buf,"gX:%.2f, gY:%.2f, gZ:%.2f\n", gx, gy, gz);
+                add_accel_value(ax, ay, az);
+                 // get smoothed values
+                float ax_s, ay_s, az_s;
+                get_avg(&ax_s, &ay_s, &az_s);
+
+                int o = orientation_from_accel(ax_s, ay_s, az_s);
+                if (o == 1) {
+                    sprintf(tx_buffer, "Orientation: Face Up\n");
+                } else if (o == 2) {
+                    sprintf(tx_buffer, "Orientation: Face Down\n");
+                } else if (o == 3) {
+                    sprintf(tx_buffer, "Orientation: Left Side\n");
+                } else if (o == 4) {
+                    sprintf(tx_buffer, "Orientation: Right Side\n");
+                } 
+
+                if (o != 0) {
+                    program_state = WRITE_UART;
+                    last_program_state = READ_GYRO;
+                }
+                
+                // Direct orientation check
+                // if (az > 7.0f) {
+                //     usb_serial_print(".\n");   // dot
+                // }
+                // else if (ax > 7.0f) {
+                //     usb_serial_print("-\n");   // dash
+                // }
+
             } else {
                 usb_serial_print("Failed to read imu data\n");
             }
@@ -340,7 +448,8 @@ int main() {
     init_display();
     rgb_led_write(0,0,0);
     clear_display();
-    gpio_set_irq_enabled_with_callback(SW1_PIN, GPIO_IRQ_EDGE_FALL, true, &btn_fxn);
+
+    gpio_set_irq_enabled_with_callback(SW1_PIN, GPIO_IRQ_EDGE_FALL, true, &right_button_gyrotask_fxn_switch_interrupt);
 
     TaskHandle_t hSensorTask, hPrintTask, hUartTask, hGyroTask, hDisplayTask, hUSB = NULL;
 
@@ -363,7 +472,7 @@ int main() {
         printf("Sensor task creation failed\n");
         return 0;
     }
-    result = xTaskCreate(print_task,  // (en) Task function
+    result = xTaskCreate(uart_write_task,  // (en) Task function
                 "print",              // (en) Name of the task 
                 DEFAULT_STACK_SIZE,   // (en) Size of the stack for this task (in words). Generally 1024 or 2048
                 NULL,                 // (en) Arguments of the task 
@@ -412,8 +521,8 @@ int main() {
 
 
     //test program states
-    // programState = READ_GYRO;
-    programState = READ_UART;
+    // program_state = READ_GYRO;
+    program_state = WAITING;
 
 
     // Initialize TinyUSB 
